46dc0cf73b905ab410ac730b0f4503767a536514
Matheus Borges <matob@live.com>
2019-12-07 11:16:14 -0300

    Adição dos demais algoritmos

---
+++ summa_mpi.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+#include "utils.c"
+
+#define ALGORITMO "summa_mpi"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Parametros invalidos, verifique...\n");
+		return -1;
+	}
+
+	int size = atoi(argv[1]);
+	char *logFile = argv[2];
+
+	// CRIAÇÃO DE MATRIZES
+	int *A = createMatrix(size, 0);
+	int *B = createMatrix(size, 1);
+	int *C = malloc(size * size * sizeof(int));
+
+	// TEMPOS
+	clock_t cpu_t1 = clock();
+	struct timeval exec_t1;
+	gettimeofday(&exec_t1, NULL);
+
+	// PROCESSAMENTO
+
+	// SAÍDAS
+	writeOutput(ALGORITMO, size, A, B, C);
+
+	clock_t cpu_t2 = clock();
+	double cpu_time = ((double)(cpu_t2 - cpu_t1)) / CLOCKS_PER_SEC;
+	struct timeval exec_t2;
+	gettimeofday(&exec_t2, NULL);
+	double exec_time = (double)(exec_t2.tv_usec - exec_t1.tv_usec) / 1000000 + (double)(exec_t2.tv_sec - exec_t1.tv_sec);
+	writeLog(logFile, ALGORITMO, size, cpu_time, exec_time);
+
+	return 0;
+}
---
+++ utils.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+unsigned long int curtime()
+{
+    struct timeval te;
+    gettimeofday(&te, NULL);
+    unsigned long time = te.tv_sec * 1000LL + te.tv_usec / 1000;
+    return time;
+}
+
+int *getAddress(int *index, int size, int m, int n)
+{
+    return index + ((size * m) + n);
+}
+
+void printMatrix(FILE *fp, int size, int *M)
+{
+    int m, n;
+    for (m = 0; m < size; m++)
+    {
+        for (n = 0; n < size; n++)
+            fprintf(fp, "%d ", *getAddress(M, size, m, n));
+        fprintf(fp, "\n");
+    }
+}
+
+int *createMatrix(int size, int subseed)
+{
+    int *A = malloc(size * size * sizeof(int));
+    int m, n, *x = A;
+    srand(587938712 + subseed);
+    for (m = 0; m < size; m++)
+        for (n = 0; n < size; n++)
+            *(x++) = rand() % 100;
+    return A;
+}
+
+void writeOutput(char *algoritmo, int size, int *A, int *B, int *C)
+{
+    unsigned long time = curtime();
+
+    char fileName[100];
+    sprintf(fileName, "output/%s_%ld.txt", algoritmo, time);
+
+    FILE *fp;
+    printf("%s\n", fileName);
+    fp = fopen(fileName, "w+");
+
+    printMatrix(fp, size, A);
+    fprintf(fp, "\n");
+    printMatrix(fp, size, B);
+    fprintf(fp, "\n");
+    printMatrix(fp, size, C);
+
+    fclose(fp);
+}
+
+void writeLog(char *fileName, char *algoritmo, int size, double allTime, double execTime)
+{
+    FILE *log;
+    log = fopen(fileName, "a");
+    fprintf(log, "%s,%d,%f,%f\n", algoritmo, size, allTime, execTime);
+    fclose(log);
+}
